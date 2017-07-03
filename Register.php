d2f07f309fcb2deeb3a5b5ec2d80dd96de1d4ccb
Pramod Weerasinghe <pramod4lk@gmail.com>
2017-07-03 20:53:07 +0530

    Another Commit!

---
+++ Register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Car Sale Site</title>
+</head>
+<body>
+  <div class="wrapper"> <!-- Start of Wrapper -->
+    <div class="container"> <!-- Start of Container -->
+      
+    </div> <!-- Start of Container -->
+  </div> <!-- End of Wrapper -->
+</body>
+</html>
